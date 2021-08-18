2f3beaa2c303dd49e0883330fed23d8c6dc69f49
Plow-Controller <89102207+Plow-Controller@users.noreply.github.com>
2021-08-17 20:27:13 -0400

    Update update.sh
    Enhancement.. Now saves plot-directory file so it is retained after updating The Plow.

--- install-files/update.sh
+++ install-files/update.sh
@@ -1,21 +1,27 @@
-#!bin/sh
+#!/bin/sh
 
+cd ~/
 echo "Uninstalling The Plow"
 sudo rm /usr/bin/plow.py
 sudo rm /usr/bin/plow_rc.py
 sudo rm -f --recursive -d /usr/share/icons/plow
 sudo rm /usr/share/applications/plow.desktop
 sudo rm ~/Desktop/plow.desktop
-cd ~/
 mkdir ~/plow-old
-sudo rm -f --recursive -d ~/plow-old/old-config/
-mv -i -f ~/plow/old-config ~/plow-old
+echo "Do you wish to overwrite a previously saved old-config directory?"
+mv -i ~/plow/old-config ~/plow-old
+echo "Do you wish to overwrite a previously saved plot-directory file?"
+mv -i ~/plow/plot-directories.yaml ~/plow-old
 sudo rm -f --recursive -d ~/plow
 echo "Uninstall Complete"
 echo "Downloading new Verion from GitHub."
 git clone https://github.com/Plow-Controller/plow.git
-cd ~/plow/install-files
 echo "Installing New Verion."
+sudo rm -f --recursive -d ~/plow/old-config
+sudo rm -f --recursive -d ~/plow/plot-directories.yaml
+mv ~/plow-old/old-config ~/plow/
+mv ~/plow-old/plot-directories.yaml ~/plow/
+cd ~/plow/install-files
 sudo cp -i plow_rc.py /usr/bin/plow_rc.py
 sudo cp -i ~/plow/plow.py /usr/bin/plow.py
 sudo cp -R -i plow /usr/share/icons/plow
